7caa9ba1db2c14a86cc7fcacf6592b0f2a34d1c4
Swcmb <85843156+Swcmb@users.noreply.github.com>
2024-07-13 01:34:19 +0800

    Add feature X

---
+++ static-site/PUSH.sh
@@ -0,0 +1,11 @@
+# 切换到本地仓库目录
+cd D:\Files\Documents\GitHub\Swcmb.github.io\static-site
+ 
+# 添加所有更改到暂存区
+git add .
+ 
+# 提交更改
+git commit -m "Add feature X"
+ 
+# 推送到远程仓库并设置上游分支
+git push -u Swcmb.github.io master
